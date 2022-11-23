7e0617d022b6e78b30e84357d3e9d86e2d62a293
Damian Korona <Damian.Korona@wp.pl>
2022-11-23 16:10:24 +0100

    Answer 2

--- 9_186  Error-handling-in-Python - instruction else i finally_Lab.py
+++ 9_186  Error-handling-in-Python - instruction else i finally_Lab.py
@@ -1,7 +1,12 @@
+import os
+import sys
 input_user = input('Acceptable price of a new course idemy. Your price? ')
 filepath = input('Enter the path to the file: ')
 
-line = input_user
-file = open(filepath, 'w')
-file.write(line)
-file.close()
+try:
+    line = input_user
+    file = open(filepath, 'w')
+    file.write(line)
+    file.close()
+except:
+    print("SORRY - WE HAVE AN ERROR", sys.exc_info())
